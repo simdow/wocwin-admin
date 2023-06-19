130103e446b1e9b301936c490275c6f9c495e43b
wocwin <wocwin@163.com>
2023-06-19 19:37:45 +0800

    deploy

---
+++ assets/js/index-22b01757.js
@@ -0,0 +1,39 @@
+import"./vue@3.2.47-e9f0a81f.js";import{d as e,Z as l,o as t,L as a,M as o,e as i,Q as s}from"./@vue_runtime-core@3.2.47-3393fa3b.js";import{c as r,k as u,u as p}from"./@vue_reactivity@3.2.47-71c1ef0d.js";import{_ as n}from"./index-08301f42.js";import"./@intlify_core-base@9.2.2-3c9acb8b.js";import"./@intlify_message-compiler@9.2.2-a38edf5b.js";import"./@intlify_shared@9.2.2-76c746da.js";import"./source-map@0.6.1-f5811eb2.js";import"./@intlify_devtools-if@9.2.2-c4c7726f.js";import"./@vue_runtime-dom@3.2.47-bb459063.js";import"./@vue_shared@3.2.47-699fd3ac.js";import"./vue-router@4.1.6_vue@3.2.47-c507d381.js";import"./@vue_devtools-api@6.5.0-d955f204.js";import"./js-cookie@3.0.1-d59fc71c.js";import"./pinia@2.0.33_typescript@5.0.4_vue@3.2.47-7bcd43e3.js";import"./vue-demi@0.14.0_vue@3.2.47-90709de6.js";import"./element-plus@2.3.3_vue@3.2.47-b480741b.js";import"./lodash-es@4.17.21-3aa1b897.js";import"./@vueuse_shared@9.13.0_vue@3.2.47-5955aa86.js";import"./@vueuse_core@9.13.0_vue@3.2.47-68996be2.js";import"./@element-plus_icons-vue@2.1.0_vue@3.2.47-fb97c814.js";import"./@sxzz_popperjs-es@2.11.7-b78c3215.js";import"./@ctrl_tinycolor@3.6.0-08719ea3.js";import"./dayjs@1.11.7-836ee5ed.js";import"./async-validator@4.2.5-f440436c.js";import"./memoize-one@6.0.0-63ab667a.js";import"./escape-html@1.0.3-e120bcc9.js";import"./normalize-wheel-es@1.2.0-3222b0a2.js";import"./@floating-ui_dom@1.2.7-73f3ed74.js";import"./@floating-ui_core@1.2.6-71d29d7a.js";import"./nprogress@0.2.0-db6fffad.js";import"./vite-plugin-qiankun@1.0.15_typescript@5.0.4_vite@4.3.9-f25e2b71.js";import"./axios@1.3.4-e3d92af1.js";import"./vue-i18n@9.2.2_vue@3.2.47-b5a03d27.js";import"./pinia-plugin-persistedstate@3.1.0_pinia@2.0.33-d2bd58cf.js";import"./@wocwin_t-ui-plus@1.1.4-a0101cea.js";const m=n(e({__name:"index",setup(e){const n=r(null),m=u({labelPosition:"right",ref:null,formData:{account:null,
+// *用户账号
+password:null,
+// *用户密码
+name:null,
+// *用户昵称
+sex:null,
+// *性别: 0:男 1:女
+hobby:[],
+// *爱好: 0:男 1:女
+phone:null,
+// 手机号码
+createDate:null,
+// 创建时间
+valDate:null,
+// el日期选择范围
+wechat:null,
+// 微信
+qq:null,
+// qq
+accountType:null,
+// *用户类型: 0: 手机注册 1: 论坛注册 2: 管理平台添加
+email:null,
+// 邮箱
+desc:null,
+// 描述
+number:null,
+// 计算器
+status:null},fieldList:[{label:"账号",value:"account",type:"input",comp:"el-input",event:"account"},{label:"密码",value:"password",type:"password",comp:"el-input"},{label:"昵称",value:"name",type:"input",comp:"el-input"},{label:"性别",value:"sex",type:"select-arr",comp:"el-select",list:"sexList",bind:()=>({disabled:!0}),arrLabel:"key",arrKey:"value"},{label:"状态",value:"status",type:"select-arr",list:"statusList",comp:"el-select",arrLabel:"key",arrKey:"value"},{label:"爱好",value:"hobby",type:"checkbox",comp:"el-checkbox-group",list:"hobbyList",event:"checkbox"},{label:"手机号码",value:"phone",type:"input",comp:"el-input",bind:{maxlength:11}},{
+// labelRender: () => {
+//   return (
+//     <div class="label_render">
+//       <div>自定义label</div>
+//     </div>
+//   )
+// },
+label:"自定义label",placeholder:"请选择element日期范围",value:"valDate",type:"daterange",comp:"el-date-picker",bind:{rangeSeparator:"-",startPlaceholder:"开始日期",endPlaceholder:"结束日期",valueFormat:"YYYY-MM-DD"}},{label:"描述",value:"desc",type:"textarea",comp:"el-input",widthSize:1}],operatorList:[{label:"提交",type:"danger",fun:()=>{m.ref.validate((e=>{}))}},{label:"重置",type:"primary",fun:()=>{m.formData={},n.value.clearValidate()}}],
+// 相关列表
+listTypeInfo:{hobbyList:[{label:"吉他",value:"0"},{label:"看书",value:"1"},{label:"美剧",value:"2"},{label:"旅游",value:"3"},{label:"音乐",value:"4"}],sexList:[{key:"女",value:1},{key:"男",value:0}],statusList:[{key:"启用",value:1},{key:"停用",value:0}]}});return(e,r)=>{const u=l("el-radio-button"),c=l("el-radio-group"),d=l("t-form"),v=l("t-layout-page-item"),b=l("t-layout-page");return t(),a(b,null,{default:o((()=>[i(v,null,{default:o((()=>[i(c,{modelValue:p(m).labelPosition,"onUpdate:modelValue":r[0]||(r[0]=e=>p(m).labelPosition=e),size:"small",style:{"margin-bottom":"15px"}},{default:o((()=>[i(u,{label:"left"},{default:o((()=>[s("左对齐")])),_:1}),i(u,{label:"right"},{default:o((()=>[s("右对齐")])),_:1}),i(u,{label:"top"},{default:o((()=>[s("顶部对齐")])),_:1})])),_:1},8,["modelValue"]),i(d,{ref_key:"TFormDemo",ref:n,modelValue:p(m).ref,"onUpdate:modelValue":r[1]||(r[1]=e=>p(m).ref=e),formOpts:p(m),widthSize:1},null,8,["modelValue","formOpts"])])),_:1})])),_:1})}}}),[["__file","F:/Wrok/Me/wocwin-admin/src/views/tuiplus/TForm/labelPosition/index.vue"]]);export{m as default};
